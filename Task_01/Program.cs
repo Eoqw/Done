64ffa22978c0be56e235eeaba25edde9fdc341d3
Denis Lodygin <Alirka94@mail.ru>
2023-12-07 22:47:46 +0500

    добавили Task01

---
+++ Task_01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Произвольный массив
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        Console.WriteLine("Элементы массива, начиная с конца:");
+        PrintArrayReversed(array, array.Length - 1);
+
+        Console.ReadLine(); // Чтобы консольное окно не закрывалось сразу
+    }
+
+    static void PrintArrayReversed(int[] arr, int index)
+    {
+        if (index >= 0)
+        {
+            Console.WriteLine(arr[index]);
+            PrintArrayReversed(arr, index - 1);
+        }
+    }
+}
