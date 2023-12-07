c7328a0388546c914119966076b010e369a7556b
Denis Lodygin <Alirka94@mail.ru>
2023-12-07 23:11:12 +0500

    Добавили task02

---
+++ Task_02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите M: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите N: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Натуральные числа в промежутке от {m} до {n}:");
+        PrintNaturalNumbers(m, n);
+
+        Console.ReadLine(); // Чтобы консольное окно не закрывалось сразу
+    }
+
+    static void PrintNaturalNumbers(int start, int end)
+    {
+        if (start <= end)
+        {
+            Console.WriteLine(start);
+            PrintNaturalNumbers(start + 1, end);
+        }
+    }
+}
